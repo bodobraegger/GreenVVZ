e4fe9155ab51523e722228ec6d8242879eb2c65e
bobrae <braegger.bodo@gmail.com>
2019-03-10 17:59:06 +0100

    SQL queries to create tables

---
+++ tables_creation.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS `whitelist` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `SmObjId` INT(8) NOT NULL, 
+    `PiqYear` INT(4) NOT NULL,
+    `PiqSession` INT(3) NOT NULL,
+    `held_in` INT(3),
+    `title` TEXT
+);
+
+CREATE TABLE IF NOT EXISTS `blacklist` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `SmObjId` INT(8) NOT NULL, 
+    `PiqYear` INT(4) NOT NULL,
+    `PiqSession` INT(3) NOT NULL,
+    `held_in` INT(3),
+    `title` TEXT
+);
+
+CREATE TABLE IF NOT EXISTS `searchterms` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `term` TEXT
+);
