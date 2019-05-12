2473e519d041b3269aeda7d99a4460d528483773
bobrae <braegger.bodo@gmail.com>
2019-05-12 18:40:07 +0200

    updated sql

--- tables_creation.sql
+++ tables_creation.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS `whitelist` (
+CREATE TABLE IF NOT EXISTS `modules` (
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `SmObjId` INT(8) NOT NULL UNIQUE, 
     `PiqYear` INT(4) NOT NULL,
     `PiqSession` INT(3) NOT NULL,
-    `held_in` INT(3),
-    `title` TEXT
+    `title` TEXT,
+    `whitelisted` BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS `blacklist` (
@@ -19,4 +19,18 @@ CREATE TABLE IF NOT EXISTS `blacklist` (
 CREATE TABLE IF NOT EXISTS `searchterms` (
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `term` TEXT NOT NULL CHECK (`term` <> N'') -- Ignored in MariaDB version < 10.2.1, we are on 10.1.37 :(
+);
+
+CREATE TABLE IF NOT EXISTS studyprograms (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ScObjId INT(8) NOT NULL UNIQUE,
+    ScText TEXT
+);
+
+CREATE TABLE IF NOT EXISTS modules_studyprograms (
+    SmObjId INT(8) NOT NULL,
+    ScObjId INT(8) NOT NULL,
+    FOREIGN KEY (SmObjId) REFERENCES modules (SmObjId) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (ScObjId) REFERENCES studyprograms (ScObjId) ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY (SmObjId, ScObjId)
 );
