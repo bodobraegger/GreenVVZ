713c9be4bbfd0482206f76aca4264e8577e44091
bobrae <braegger.bodo@gmail.com>
2019-05-15 16:30:31 +0200

    new table schemas

--- tables_creation.sql
+++ tables_creation.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS `modules` (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS modules (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     SmObjId INT(8) NOT NULL, 
     PiqYear INT(4) NOT NULL,
     PiqSession INT(3) ZEROFILL NOT NULL,
@@ -8,10 +8,9 @@ CREATE TABLE IF NOT EXISTS `modules` (
     CONSTRAINT key_2 UNIQUE (SmObjId, PiqYear, PiqSession)
 );
 
--- should be searchterms
-CREATE TABLE IF NOT EXISTS `searchterms` (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `term` TEXT NOT NULL CHECK (`term` <> N'') -- Ignored in MariaDB version < 10.2.1, we are on 10.1.37 :(
+CREATE TABLE IF NOT EXISTS searchterms (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    term TEXT NOT NULL CHECK (term <> N'') -- Ignored in MariaDB version < 10.2.1, we are on 10.1.37 :(
 );
 
 CREATE TABLE IF NOT EXISTS studyprograms (
