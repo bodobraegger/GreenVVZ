61c69f4c055877b94f813958012f85734359afb7
bobrae <braegger.bodo@gmail.com>
2019-06-02 11:27:33 +0200

    document public.js

--- static/public.js
+++ static/public.js
@@ -1,9 +1,7 @@
-var baseUrlVvzUzh = 'https://studentservices.uzh.ch/uzh/anonym/vvz/index.html#/details/'
-var apiUrl = 'https://greenvvz.ifi.uzh.ch'
-
+/* REQUIRES admin.js TO BE LOADED BEFOREHAND, FOR apiUrl, baseUrlVvzUzh and ShowSelectedModules() */
 $(document).ready(function () {
-    //https://studentservices.uzh.ch/uzh/anonym/vvz/index.html#/details/2017/004/SM/50825256
     var root = $('#anchor-public')
+    // if a data-lang tag exists
     if(root.data("lang") === 'en'){
         var langTitle = 'UZH Modules related to sustainability';
         var langName = 'Name of the Module';
@@ -13,16 +11,20 @@ $(document).ready(function () {
         var langName = 'Name des Moduls';
         var langSemester = '(FS = Frühjahressemester, HS = Herbstsemester)'
     }
-
+    // load the whitelist elements into the page, using the anchor-public div
     $.ajax({
+        // apiUrl is defined in admin.js
         url : apiUrl+"/modules/whitelist",
         method : 'GET',
         success : function (data) {
+            // create a table, 
             var table = $("<table></table>")
             table.append('<thead><th colspan="2"><strong>'+langTitle+'</strong></th></thead>')
             var body = $('<tbody id="whitelist_body"></tbody>')
             body.append('<tr><td><strong>'+langName+'</strong></td><td><strong>Semester</strong><br><p>'+langSemester+'</p></td></tr>')
+            // append the modules as rows with proper id, data-SmObjId, data-semester, and a link to the vvz
             for (var row in data){
+                // baseUrlVvzUzh is defined in admin.js
                 var url = baseUrlVvzUzh+data[row].PiqYear+'/'+data[row].PiqSession+'/SM/'+data[row].SmObjId
                 var module = $(`
                 <tr id="module_${data[row].id}" data-SmObjId="${data[row].SmObjId}" data-semester="${data[row].PiqYear} ${data[row].PiqSession}" class="shown">
@@ -32,6 +34,7 @@ $(document).ready(function () {
             }
             table.append(body)
             root.append(table)
+            // show the modules for current semester - defined in admin.js
             ShowSelectedModules();
         },
         error : function (err) {
@@ -39,7 +42,12 @@ $(document).ready(function () {
         }
     })
 });
-
+/**
+ * Flag tablerows for modules in the semester chosen by the dropdown selector "semester".
+ * @param  {Number} session The session code, either 3, 4, 003, 004.
+ * @param  {Number} year the module year data.
+ * @return {String} A span containing the humanreadable semester and year.
+ */
 function convert_session_to_string(session, year){
     if (session == 3){
         return `<span class="semester">HS </span>${year % 100 || ''}`
