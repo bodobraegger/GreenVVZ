26d8b1d4c67f9263b53fad296e32cac68ab84563
bobrae <braegger.bodo@gmail.com>
2019-09-11 18:34:50 +0200

    only update newest semester

--- updateModules.py
+++ updateModules.py
@@ -20,7 +20,8 @@ def update_modules() -> bool:
     # delete modules of semester no longer relevant (as defined by the default num_prev_semesters+1, or len(helpers.get_current_sessions())-2 +1
     no_longer_relevant_session = helpers.get_current_sessions(len(helpers.get_current_sessions())-1)[-1]
     cursor.execute("DELETE FROM module WHERE PiqYear = %(year)s AND PiqSession = %(session)s", no_longer_relevant_session)
-    cursor.execute("SELECT * FROM module")
+    last_relevant_session = helpers.get_current_sessions()[1]
+    cursor.execute("SELECT * FROM module WHERE PiqYear = %(year)s AND PiqSession = %(session)s", last_relevant_session)
     for row in cursor:
         # current semester update
         cursor2 = cnx.cursor()
